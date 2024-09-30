6aedb0c37fa9d7b701424fcf5a07dac660f30198
z64me <45853441+z64me@users.noreply.github.com>
2024-09-30 12:57:01 -0400

    update compiler paths in win32 build script
    No longer using hardcoded paths

--- release-win32.sh
+++ release-win32.sh
@@ -2,15 +2,15 @@
 mkdir -p o
 
 # build compression functions (slow)
-~/c/mxe/usr/bin/i686-w64-mingw32.static-gcc -c -DNDEBUG -s -Ofast -flto -lm -Wall src/enc/*.c src/enc/lzo/*.c src/enc/ucl/comp/*.c src/enc/apultra/*.c
+i686-w64-mingw32.static-gcc -c -DNDEBUG -s -Ofast -flto -lm -Wall src/enc/*.c src/enc/lzo/*.c src/enc/ucl/comp/*.c src/enc/apultra/*.c
 mv *.o o
 
 # build stb functions (slow)
-~/c/mxe/usr/bin/i686-w64-mingw32.static-gcc -c -DNDEBUG src/stb/*.c -Wall -lm -s -Os -flto -lpthread -Wno-unused-function -Wno-unused-variable
+i686-w64-mingw32.static-gcc -c -DNDEBUG src/stb/*.c -Wall -lm -s -Os -flto -lpthread -Wno-unused-function -Wno-unused-variable
 mv *.o o
 
 # build everything else
-~/c/mxe/usr/bin/i686-w64-mingw32.static-gcc -o zzrtl.exe -DNDEBUG src/*.c o/*.o -Wall -lm -s -Os -flto -lpthread -Wno-unused-function -Wno-unused-variable
+i686-w64-mingw32.static-gcc -o zzrtl.exe -DNDEBUG src/*.c o/*.o -Wall -lm -s -Os -flto -lpthread -Wno-unused-function -Wno-unused-variable
 
 # move to bin directory
 mkdir -p bin/win32
